d1a5365024c52c2cdc54b137589ed37eab3a93a3
alowh <francesco.hayes@alowh.com>
2019-04-13 11:22:20 -0400

    Added gsap and three with clean files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import * as THREE from 'three'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
+  },
+  beforeMount () {
+    
+  },
+  methods: {
+
   }
 }
 </script>
